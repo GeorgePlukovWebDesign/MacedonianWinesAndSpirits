044c763ab1f1f43bf35f2933459b5f2a9df87fff
GeorgePlukov <gplukov@gmail.com>
2017-05-12 23:42:42 -0400

    Initial dist subtree commit

--- web-frontend/src/components/Dynatitle.vue
+++ web-frontend/src/components/Dynatitle.vue
@@ -1,7 +1,9 @@
 <template id="dynatitle">
 
   <h1 class="title">
-    <div v-for="word in words"></div>
+    <span v-for="word in words">
+      <span class="letter">{{word[0]}}</span><span class="word">{{word[1]}}</span>
+    </span>
 
   </h1>
 </template>
@@ -10,7 +12,7 @@ export default {
   name: 'dynatitle',
   data() {
     return {
-      words: ["Macedonian", "Wines", "&","Spirits"]
+      words: [["M","acedonian "], ["W","ines "], ["& "], ["S","pirits "]]
     }
   }
 }
@@ -19,21 +21,50 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .title{
+  margin-left: 10px;
+  padding-right: 5px;
   /* Positioning */
   position: sticky;
   top: 50%;
   left: 50%;
-  width: 100%;
-  max-width: 450px;
-
-  transform: translate(-50%,-50%);
+  /*width: 100%;*/
+  max-width: 40%;
 
   /* Styling */
   text-align: left;
-  font-size: 3em;
+  font-size: 10em;
   font-weight: normal;
 
+  /* translations and animations */
+  transform: translate(-50%,-50%);
+  transition: transform .4s ease;
+  transition: font-size 4s ease;
+
+  /*font-size*/
 
 
 }
+@media (max-width: 1200px) {
+  .title{
+    font-size: 5em;
+    transition: transform .6s;
+    transition: font-size .2s ease;
+
+  }
+}
+@media (max-width: 630px) {
+  .title{
+    /* Positioning */
+    max-width: 100%;
+    width:100%;
+    font-size: 2em;
+
+    /* translations and animations */
+    transition: transform .6s;
+    transition: font-size .2s ease;
+
+    transform: translateY(-50%);
+  }
+}
+
 </style>
