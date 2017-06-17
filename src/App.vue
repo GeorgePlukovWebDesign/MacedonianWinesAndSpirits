1d38b3efcd24fb07a657b2c40e05afecbaba8782
GeorgePlukov <gplukov@gmail.com>
2017-06-17 17:17:26 -0400

    Add: github starter tab, Add: instagram auto image load tab

--- src/App.vue
+++ src/App.vue
@@ -5,36 +5,36 @@
             <span class="lato-hairline">George </span><span class="lato-bold">Plukov</span>
         </div>
         <div class="menu raleway">
-            <a v-on:mouseover="mouseOver(0)"  v-on:mouseleave="mouseLeave(0)"href="http://www.google.com">Gallery</a>
-            <a v-on:mouseover="mouseOver(1)"  v-on:mouseleave="mouseLeave(1)" href="#">Blog</a>
-            <a v-on:mouseover="mouseOver(2)"  v-on:mouseleave="mouseLeave(2)" href="#">
+            <a v-on:mouseover="mouseOver(0)" v-on:mouseleave="mouseLeave(0)" href="#">Gallery</a>
+            <a v-on:mouseover="mouseOver(1)" v-on:mouseleave="mouseLeave(1)" href="#">Blog</a>
+            <a v-on:mouseover="mouseOver(2)" v-on:mouseleave="mouseLeave(2)" href="#">
               Github
               <!-- <img class="github" src="./assets/GitHub-Mark-120px.png" alt="Link to George Plukov's github page. www.github.com/GeorgePlukov"> -->
             </a>
-            <a v-on:mouseover="mouseOver(3)"  v-on:mouseleave="mouseLeave(3)" href="#">Pinterest</a>
-            <a v-on:mouseover="mouseOver(4)"  v-on:mouseleave="mouseLeave(4)" href="#">Insta
+            <a v-on:mouseover="mouseOver(3)" v-on:mouseleave="mouseLeave(3)" href="#">Pinterest</a>
+            <a v-on:mouseover="mouseOver(4)" v-on:mouseleave="mouseLeaveButton(4)" href="#">Insta
 <!-- <img class="github" src="./assets/Insta-Mark-120px.png" alt="Link to George Plukov's github page. www.github.com/GeorgePlukov"> -->
             </a>
 
 
         </div>
         <transition name="slide-fade">
-            <div v-if="current_active != -1"  class="preview">
+            <div v-if="current_active != -1" class="preview">
                 <div class="lineblock">
                     <!-- Line that fits the gallery item -->
-                  <svg v-show="current_active == 0" id="line-gallery" class="line" height="10" width="100%">
+                    <svg v-show="current_active == 0" id="line-gallery" class="line" height="10" width="100%">
                     <line x1="0px" y1="10px" x2="2em" y2="10px"  />
                     <line class="lighter" x1="2em" y1="10px" x2="2.5em" y2="0px" />
                     <line class="lighter" x1="2.5em" y1="0px" x2="3em" y2="10px" />
                     <line x1="3em" y1="10px" x2="83%" y2="10px" />
                   </svg>
-                  <div v-show="current_active == 0" class="todo1">
-                      <div class="item-box">
-                      </div>
-                      <div class="item-box">
-                      </div>
-                      <div class="item-box">
-                      </div>
+                  <div v-show="current_active == 0" v-on:mouseleave="mouseLeave(0)" class="todo1">
+
+                      <!-- <img class="preview-image" v-bind:src="" alt=""> -->
+
+                      <!-- <img class="preview-image" v-bind:src="" alt=""> -->
+
+                      <img class="preview-image" v-bind:src="insta_data['items'][2]['images']['standard_resolution']['url']" alt="">
                   </div>
 
 
@@ -44,24 +44,55 @@
                     <line class="lighter" x1="6.75em" y1="0px" x2="7.25em" y2="10px" />
                     <line x1="7.25em" y1="10px" x2="83%" y2="10px" />
                   </svg>
+
+                  <!-- Github -->
                   <svg v-show="current_active == 2" id="line-github" class="line" height="10" width="100%">
                     <line x1="0px" y1="10px" x2="10em" y2="10px"  />
                     <line class="lighter" x1="10em" y1="10px" x2="10.5em" y2="0px" />
                     <line class="lighter" x1="10.5em" y1="0px" x2="11em" y2="10px" />
                     <line x1="11em" y1="10px" x2="83%" y2="10px" />
                   </svg>
-                  <svg v-show="current_active == 3" id="line-pinterest" class="line" height="10" width="100%">
+                  <div v-show="current_active == 2" v-on:mouseleave="mouseLeave(2)" class="todo1">
+
+                      <!-- <img class="preview-image" v-bind:src="" alt=""> -->
+
+                      <!-- <img class="preview-image" v-bind:src="" alt=""> -->
+
+                      <img class="preview-image" v-bind:src="insta_data['items'][2]['images']['standard_resolution']['url']" alt="">
+                  </div>
+
+
+
+                    <svg v-show="current_active == 3" id="line-pinterest" class="line" height="10" width="100%">
                     <line x1="0px" y1="10px" x2="15em" y2="10px"  />
                     <line class="lighter" x1="15em" y1="10px" x2="15.5em" y2="0px" />
                     <line class="lighter" x1="15.5em" y1="0px" x2="16em" y2="10px" />
                     <line x1="16em" y1="10px" x2="83%" y2="10px" />
                   </svg>
-                  <svg v-show="current_active == 4" id="line-instagram" class="line" height="10" width="100%">
+                  <!-- <div v-show="current_active == 3" v-on:mouseleave="mouseLeave(3)" class="todo1">
+
+                    <a data-pin-do="embedBoard" data-pin-board-width="400" data-pin-scale-height="240" data-pin-scale-width="80" href="https://www.pinterest.com/pinterest/official-news/"></a>
+
+                      - <img class="preview-image" v-bind:src="insta_data['items'][1]['images']['standard_resolution']['url']" alt="">
+
+                      <img class="preview-image" v-bind:src="insta_data['items'][2]['images']['standard_resolution']['url']" alt=""> ->
+                  </div> -->
+
+                    <!-- instagram -->
+                    <svg v-show="current_active == 4" id="line-instagram" class="line" height="10" width="100%">
                     <line x1="0px" y1="10px" x2="19.5em" y2="10px"  />
                     <line class="lighter" x1="19.5em" y1="10px" x2="20em" y2="0px" />
                     <line class="lighter" x1="20em" y1="0px" x2="20.5em" y2="10px" />
                     <line x1="20.5em" y1="10px" x2="83%" y2="10px" />
                   </svg>
+                    <div v-show="current_active == 4" v-on:mouseleave="mouseLeave(4)" class="todo1">
+
+                        <img class="preview-image" v-bind:src="insta_data['items'][0]['images']['standard_resolution']['url']" alt="">
+
+                        <img class="preview-image" v-bind:src="insta_data['items'][1]['images']['standard_resolution']['url']" alt="">
+
+                        <img class="preview-image" v-bind:src="insta_data['items'][2]['images']['standard_resolution']['url']" alt="">
+                    </div>
                 </div>
 
             </div>
@@ -75,16 +106,37 @@ export default {
     name: 'app',
     data() {
         return {
-            name: 'george',
-            current_active: -1
+            current_active: -1,
+            continue: false,
+            insta_data: null
         }
+    },
+    created: function() {
+        this.axios.get('https://www.instagram.com/georgeplukov/media/').then((response) => {
+            this.insta_data = response.data
+
+        })
     },
     methods: {
         mouseOver: function(num) {
-          this.current_active = num
+            this.current_active = num;
+
         },
-        mouseLeave: function(num){
-          this.current_active = -1;
+        mouseLeaveButton: function(num) {
+
+          setTimeout(function() {
+            // if (this.current_active != )
+            this.current_active = -1;
+
+          }, 50);
+
+        },
+        mouseLeave: function(num) {
+
+
+            this.current_active = -1;
+
+
         }
     }
 }
@@ -157,22 +209,36 @@ line {
     transition: all 0.2s;
 }
 /* .slide-fade-leave-active for <2.1.8 */
-.slide-fade-enter{
+.slide-fade-enter {
     transform: translateY(40%);
-    opacity:0;
+    opacity: 0;
 }
 .slide-fade-leave-to {
     opacity: 0;
 
 }
-.item-box{
-  display: inline-block;
-  height: 100%;
-  min-height: 150px;
-  margin-left: 10px;
-  margin-right: 5px;
-  width: 25%;
-  border: 1px solid lightgrey;
-  border-radius: 10px;
+.item-box {
+    display: inline-block;
+    // max-height: 10vh;
+    height: auto;
+    width: 25%;
+
+    // max-height: 150px;
+    margin-left: 10px;
+    margin-right: 5px;
+    border: 1px solid lightgrey;
+    border-radius: 10px;
+}
+.preview-image {
+    height: auto;
+    width: 25%;
+
+    display: inline-block;
+
+    // max-height: 150px;
+    margin-left: 10px;
+    margin-right: 5px;
+    border: 1px solid lightgrey;
+    border-radius: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import ghPinnedRepos from 'gh-pinned-repos'
 
+Vue.use(VueAxios, axios)
+ghPinnedRepos('georgeplukov')
+  .then(console.log) // [...]
 new Vue({
   el: '#app',
   render: h => h(App)
